b1cb082c9c8aa5fe5e5f443599e54413a5dec919
SvyatoslavMedov <irobot1204@gmail.com>
2021-07-19 12:24:58 +0600

    Initial commit

---
+++ src/main/java/DiscardServerHandler.java
@@ -0,0 +1,34 @@
+
+
+import io.netty.channel.socket.SocketChannel;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead (ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) {
+                System.out.println((char) in.readByte());
+                System.out.flush();
+            }
+        }finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught (ChannelHandlerContext ctx, Throwable cause) {
+
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
